84df338b5bd2b74807405322dfff94a4f42b94a3
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-03-16 22:25:19 +0530

    Task04 completed

---
+++ Activity/Task04.java
@@ -0,0 +1,19 @@
+/*
+public class Task04{
+    public static void main(String[] args){
+        for(int i=1;i<=20;i=i+2){
+            System.out.println(i);
+        }
+    } 
+}
+*/
+
+public class Task04{
+    public static void main(String[] args){
+          for (int i=1;i<=20;i++){
+            if(i%2==1){
+                System.out.println(i);
+            }
+          }
+    }
+}
